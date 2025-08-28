e885dfc56777d94671318bd588e860a2a3876ed1
Mohammed Sharif <muhammedsharif2007@gmail.com>
2025-08-28 12:31:50 +0530

    Data Transformation Completed: Generated output.csv with Sales, Date, Region

---
+++ transform.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import glob
+import os
+
+print("Script started...")
+
+data_path = os.path.join("data", "*.csv")
+all_files = glob.glob(data_path)
+df_list = [pd.read_csv(file) for file in all_files]
+df = pd.concat(df_list, ignore_index=True)
+
+df = df[df["product"].str.lower() == "pink morsel"]
+
+df["price"] = df["price"].replace('[\$,]', '', regex=True).astype(float)
+df["quantity"] = pd.to_numeric(df["quantity"], errors="coerce")
+
+df["sales"] = df["quantity"] * df["price"]
+
+output_df = df[["sales", "date", "region"]]
+output_df.to_csv("output.csv", index=False)
+
+print("Output saved as output.csv")
